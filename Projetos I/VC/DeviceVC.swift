c979254bdf7034fcd52f342bb698c8acbab3df73
Isaías <isaiahlima18@gmail.com>
2018-04-21 22:59:53 -0300

    Atualiza closed e delays

--- Projetos I/VC/DeviceVC.swift
+++ Projetos I/VC/DeviceVC.swift
@@ -18,8 +18,20 @@ class DeviceVC: UITableViewController {
             self.topicLabel.text = d.topic
             self.closedLabel.text = "\(d.closed)"
             self.activeLabel.text = "\(d.working)"
-            self.onLabel.text = "\(d.onDelay) s"
-            self.offLabel.text = "\(d.offDelay) s"
+//            self.onLabel.text = "\(d.onDelay) s"
+//            self.offLabel.text = "\(d.offDelay) s"
+
+            var index: Int = 0
+            if d.onDelay != self.onTimes[self.onSelected /* self.onDelayPV.selectedRow(inComponent: 0) */] {
+                index = self.onTimes.index(of: d.onDelay) ?? 0
+                self.onDelayPV.selectRow(index, inComponent: 0, animated: true)
+                self.onSelected = index
+            }
+            if d.offDelay/60 != self.offTimes[self.offSelected /*self.offDelayPV.selectedRow(inComponent: 0) */] {
+                index = self.offTimes.index(of: d.offDelay/60) ?? 0
+                self.offDelayPV.selectRow(index, inComponent: 0, animated: true)
+                self.offSelected = index
+            }
 
             self._switch.isOn = d.closed
 
@@ -37,13 +49,21 @@ class DeviceVC: UITableViewController {
     @IBOutlet weak var topicLabel: UILabel!
     @IBOutlet weak var closedLabel: UILabel!
     @IBOutlet weak var activeLabel: UILabel!
-    @IBOutlet weak var onLabel: UILabel!
-    @IBOutlet weak var offLabel: UILabel!
+//    @IBOutlet weak var onLabel: UILabel!
+//    @IBOutlet weak var offLabel: UILabel!
     @IBOutlet weak var _switch: UISwitch!
+    @IBOutlet weak var onDelayPV: UIPickerView!
+    @IBOutlet weak var offDelayPV: UIPickerView!
     
     fileprivate var manager: SocketManager!
     fileprivate var socket: SocketIOClient!
     fileprivate var spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    fileprivate let onTimes: [Double] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    fileprivate let offTimes: [Double] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+    fileprivate var onSelected: Int = 0
+    fileprivate var offSelected: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,10 +72,17 @@ class DeviceVC: UITableViewController {
         self.spinner.hidesWhenStopped = true
         self.navigationItem.rightBarButtonItems = [item]
 
+        self.onDelayPV.delegate = self
+        self.onDelayPV.dataSource = self
+        self.offDelayPV.delegate = self
+        self.offDelayPV.dataSource = self
+
         self.manager = SocketManager(socketURL: URL(string: "https://projetos-eletronicos.herokuapp.com/")!, config: [.log(true), .compress])
         self.socket = self.manager.defaultSocket
 
         self._switch.isUserInteractionEnabled = false
+        self.onDelayPV.isUserInteractionEnabled = false
+        self.offDelayPV.isUserInteractionEnabled = false
         self.socket.connect()
 
         self.socket.on(clientEvent: .connect) { (data, ack) in
@@ -66,6 +93,8 @@ class DeviceVC: UITableViewController {
         self.socket.on("device") { (dev, ack) in
             self.spinner.stopAnimating()
             self._switch.isUserInteractionEnabled = true
+            self.onDelayPV.isUserInteractionEnabled = true
+            self.offDelayPV.isUserInteractionEnabled = true
             print(#function, dev, ack)
             let json = JSON(dev[0])
             guard let d = Device(json: json) else {
@@ -118,4 +147,62 @@ class DeviceVC: UITableViewController {
         super.viewDidDisappear(animated)
         self.socket.disconnect()
     }
+}
+
+extension DeviceVC: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView {
+        case self.onDelayPV:
+            return self.onTimes.count
+        case self.offDelayPV:
+            return self.offTimes.count
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView {
+        case self.onDelayPV:
+            return "\(self.onTimes[row])"
+        case self.offDelayPV:
+            return "\(self.offTimes[row])"
+        default:
+            return ""
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView {
+        case self.onDelayPV:
+            self.spinner.startAnimating()
+            ServerManager.message(topic: self.data["device_id"] ?? "fake", key: "onDelay", value: "\(self.onTimes[row])", fake: false) { (status) in
+                switch status {
+                case .success(let dev):
+                    self.onSelected = row
+                    print(#function, dev)
+                case .failure(let error):
+                    print(#function, error.localizedDescription)
+                }
+            }
+        case self.offDelayPV:
+            ServerManager.message(topic: self.data["device_id"] ?? "fake", key: "offDelay", value: "\(self.offTimes[row]*60)", fake: false) { (status) in
+                switch status {
+                case .success(let dev):
+                    self.offSelected = row
+                    print(#function, dev)
+                case .failure(let error):
+                    print(#function, error.localizedDescription)
+                }
+            }
+        default:
+            print(#function)
+        }
+    }
+
 }
